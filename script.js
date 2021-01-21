f9fb75c6fb027af1c4e8d9483791360ba9536e2a
Rylzgebalg <49571962+Rylzgebalg@users.noreply.github.com>
2021-01-22 00:07:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-function useClock(){
-    var today = new Date();
 
-    var hours = today.getHours();
-    var minutes = today.getMinutes();
-    var ampm = " AM";
-    if(parseInt(hours) > 12){
-        hours = "0" + (parseInt(hours) -12).toString();
-        ampm =" PM";
-    }else if(parseInt(hours) == 12){
-        hours = "00";
+//Vue für Desktop
+const Counter = {
+    data() {
+      return {
+        counter: 6,
+        source: 'arrow.png'
+        }
     }
-    
-    if(parseInt(minutes) < 10){
-        minutes = "0"+minutes;
-    }
-
-    var date1 = hours+':'+minutes + ampm;
-    var date2 = today.getHours()+':'+minutes+":"+today.getSeconds() + ampm;
-    document.getElementById("clock").innerHTML = date1;
-    
 }
+  
+Vue.createApp(Counter).mount('#desktop')
+
+
+
+
+
+//Vue für die Taskleiste  
+Vue.createApp({
+        data() {
+          return {
+
+            timer: 'a'
+            }
+        },
+        created() {
+
+            //Keeps clock up to date
+            this.timer = setInterval(function(){
+                var timeNow = new Date()
+                var hour = timeNow.getHours() > 9 ? timeNow.getHours().toString() : "0"+ timeNow.getHours().toString()  
+                var minute = timeNow.getMinutes() > 9 ? timeNow.getMinutes().toString() : "0"+ timeNow.getMinutes().toString()  
+
+                this.timer = hour+":"+minute
+                return hour+":"+minute
+            }.bind(this), 1000);
+
+        },
+
+
+        methods: {
+
+        },
+
+        computed: {
+
+
+        }
+}).mount('#taskbar')
+
+
